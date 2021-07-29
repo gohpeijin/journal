743a3fe285f1ae3e4d67b3583dfbe48e8c0734a1
gohpeijin <gohpeijin@gohpeijins-Mac.local>
2021-07-29 12:17:14 -0700

    chore- add comments

--- Journal/AddJournal/AddJournalViewController.swift
+++ Journal/AddJournal/AddJournalViewController.swift
@@ -12,70 +12,71 @@ class AddJournalViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet weak var buttonSubmit: UIButton!
     @IBOutlet weak var labelDate: UILabel!
-    let dateFormatter = DateFormatter()
-    let now = Date()
-//    var journal:JournalRecord?
-    
     @IBOutlet weak var labelEmptyThought: UILabel!
     @IBOutlet weak var imageEmotionSelected: UIImageView!
     @IBOutlet weak var labelEmotionSelected: UILabel!
-
     @IBOutlet weak var textViewThought: UITextView!
+    
+    let dateFormatter = DateFormatter()
+    let now = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // set the date to now
         let now = Date()
         dateFormatter.dateFormat="dd MMM yyyy"
         labelDate.text = dateFormatter.string(from: now)
         
         textViewThought.delegate = self
         
+        labelEmptyThought.isHidden = true
+        
         // to have the text view selected as soon as the view loads
         textViewThought.becomeFirstResponder()
         textViewDefaultState()
         
-        labelEmptyThought.isHidden = true
-        buttonSubmit.layer.borderColor = UIColor.init(red: 32/255, green: 176/255, blue: 156/255, alpha: 1).cgColor // popup ok buttong effect
+        // confirm button effect
+        buttonSubmit.layer.borderColor = UIColor.init(red: 32/255, green: 176/255, blue: 156/255, alpha: 1).cgColor
         
     }
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "toEditDate"{
-            let editDateVC = segue.destination as! EditDateViewController
-            editDateVC.dateSelected = labelDate.text
-        }
-        else if segue.identifier == "toEditEmotion"{
-            let editEmotionVC = segue.destination as! EditEmotionViewController
-            editEmotionVC.emotionSelected = labelEmotionSelected.text!
-        }
+    func textViewDefaultState(){
+        textViewThought.text = "Type your thought here... (max 1000 characters)"
+        textViewThought.textColor = UIColor.lightGray
+        // set the pointer to the beginning of text view
+        textViewThought.selectedTextRange = textViewThought.textRange(from: textViewThought.beginningOfDocument, to: textViewThought.beginningOfDocument)
     }
+    
     
     // add placeholder to the text view, show it when empty string even is selected
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
         labelEmptyThought.isHidden = true
         let currentText: String = textViewThought.text
-        let updatedText = ( currentText as NSString).replacingCharacters(in: range, with: text)
+        // combine current text view's text with replacement text
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
         
-        if updatedText.isEmpty{
-           textViewDefaultState()
+        if (updatedText.isEmpty){
+            textViewDefaultState()
         }
-        else if textViewThought.textColor == UIColor.lightGray && !text.isEmpty {
-             textViewThought.textColor = UIColor.black
+        else if (textViewThought.textColor == UIColor.lightGray && !text.isEmpty) {
+            textViewThought.textColor = UIColor.black
             textViewThought.text = text
         }
         else
         {
-            return self.textLimit(existingText: textView.text,newText: text,limit: 1000)
+            return self.textLimit(currentText: textView.text,newText: text,limit: 1000)
         }
         return false
     }
     // set the text limit to 1000 character
-    private func textLimit(existingText: String?, newText: String,limit: Int) -> Bool {
-        let text = existingText ?? ""
-        let isAtLimit = text.count + newText.count <= limit
-        return isAtLimit
+    private func textLimit(currentText: String?, newText: String,limit: Int) -> Bool {
+        let text = currentText ?? ""
+        let isLimitReached = text.count + newText.count <= limit
+        return isLimitReached
     }
+    
+    // prevent user changing the pointer position in text view
     func textViewDidChangeSelection(_ textView: UITextView) {
         if self.view.window != nil{
             if textViewThought.textColor == UIColor.lightGray{
@@ -84,19 +85,26 @@ class AddJournalViewController: UIViewController, UITextViewDelegate {
         }
     }
     
-    func textViewDefaultState(){
-        textViewThought.text = "Type your thought here... (max 1000 characters)"
-        textViewThought.textColor = UIColor.lightGray
-        textViewThought.selectedTextRange = textViewThought.textRange(from: textViewThought.beginningOfDocument, to: textViewThought.beginningOfDocument)
+    // pass data to next view
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toEditDate"{
+            let editDateVC = segue.destination as! EditDateViewController
+            editDateVC.dateSelected = labelDate.text
+        }
+        else if segue.identifier == "toEditEmotion"{
+            let editEmotionVC = segue.destination as! EditEmotionViewController
+            editEmotionVC.emotionSelected = labelEmotionSelected.text
+        }
     }
-    
    
     
     @IBAction func submitPressed(_ sender: UIButton) {
+        // if thought is empty, error label will appear to notify user
         if textViewThought.textColor == UIColor.lightGray{
             labelEmptyThought.isHidden = false
         }
         else{
+            // add the new reocrd to the tableview in JournalRecordViewController
             let addedJournal = JournalRecord(emotion: labelEmotionSelected.text!,thought: textViewThought.text,date: labelDate.text!)
             
             let journalRecordVC = presentingViewController as! JournalRecordViewController
@@ -108,6 +116,7 @@ class AddJournalViewController: UIViewController, UITextViewDelegate {
         
     }
     
+    // dismiss view without doing anything
     @IBAction func backPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
--- Journal/EditEmotion/EditEmotionViewController.swift
+++ Journal/EditEmotion/EditEmotionViewController.swift
@@ -11,11 +11,14 @@ import UIKit
 class EditEmotionViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
-    var searchEmotion = [String]()
-    var searching = false
-    var emotionList = [String]()
-    var emotionSelected: String = ""
-    @IBOutlet weak var emotionTableView: UITableView!
+     @IBOutlet weak var emotionTableView: UITableView!
+    
+    var searching = false           // for table view filter when true
+    var searchEmotion = [String]()  // new array list to store the search result, so table view will updated to the new list
+    var emotionList = [String]()    // list to store all the emotion of this app
+    
+    var emotionSelected: String! // use to pass the addJounalViewController's emotion to this view
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,10 +47,10 @@ class EditEmotionViewController: UIViewController {
                     // optional array of Strings
                     let emotions = plistObject as? [String]
                     
-                    // print values of array
+                    // assign values of array to a list
                     if let emotions = emotions {
                         emotionList = emotions
-                        emotionList = emotionList.sorted()
+                        emotionList = emotionList.sorted() // so the emotions in alphabetically order
                     }
                 } catch {
                     print("Error serializing data from property list")
@@ -60,6 +63,7 @@ class EditEmotionViewController: UIViewController {
         }
     }
     
+    // dismiss view without doing anything
     @IBAction func backPressed(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
@@ -68,13 +72,24 @@ class EditEmotionViewController: UIViewController {
 extension EditEmotionViewController: UITableViewDelegate{
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // pass the selected index's emotion to the addJounalViewController
         let resultList = (searching ? searchEmotion : emotionList)
         let selectedEmotion = resultList[indexPath.row]
+        
         let addJournalVC = presentingViewController as! AddJournalViewController
         addJournalVC.labelEmotionSelected.text = selectedEmotion
         addJournalVC.imageEmotionSelected.image = UIImage(named: selectedEmotion.lowercased())
         
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // let the previous default or selected emotion to be shown as selected in tableview
+        let resultList = (searching ? searchEmotion : emotionList)
+        let index = resultList.firstIndex(of: emotionSelected)
+        if(index == indexPath.row){
+            tableView.selectRow(at: indexPath, animated: false, scrollPosition: .none)
+        }
     }
 }
 
@@ -94,14 +109,15 @@ extension EditEmotionViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: EmotionTableViewCell.identifier, for: indexPath) as! EmotionTableViewCell
         
+        // change the selected background color to light green
         let backgroundview = UIView()
         backgroundview.backgroundColor = UIColor.init(red: 81/255, green: 194/255, blue: 175/255, alpha: 0.2)
         cell.selectedBackgroundView = backgroundview
         
+        // set the cell's objects
         let resultList = (searching ? searchEmotion : emotionList)
-
-        
         let selectedEmotion = resultList[indexPath.row]
+
         cell.labelEmotionName.text = selectedEmotion
         cell.imageEmotion.image = UIImage(named: selectedEmotion.lowercased())
         return cell
@@ -110,13 +126,22 @@ extension EditEmotionViewController: UITableViewDataSource{
 
 extension EditEmotionViewController: UISearchBarDelegate{
     
-    
     func searchBar(_ searchBar:UISearchBar, textDidChange searchText:String){
-        searchEmotion = emotionList.filter({$0.lowercased().contains(searchText.lowercased())})
-//        searchEmotion = emotionList.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()})
-        searching = true
+        // if no text it will set to default view
+        if (searchText == ""){
+            searching = false
+        }
+        else{
+            // match the search string with the emotion array
+            searchEmotion = emotionList.filter({$0.lowercased().contains(searchText.lowercased())})
+//        searchEmotion = emotionList.filter({$0.lowercased().prefix(searchText.count) == searchText.lowercased()}) // this will only math with same index
+            searching = true
+        }
+
         emotionTableView.reloadData()
     }
+    
+    // reset the searchbar
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searching = false
         searchBar.text = ""
